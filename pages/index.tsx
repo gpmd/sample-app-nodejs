2560dc801898c18fd923d386a16bfa61699c3588
Matt Bailey <matt@gpmd.co.uk>
2022-02-24 15:44:50 +0000

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,7 @@ const Index = () => {
 
     return (
         <Panel header="Homepage">
-            <Text marginBottom="none">{data.data}</Text>
+            <Text marginBottom="none">data</Text>
         </Panel>
     );
 };
