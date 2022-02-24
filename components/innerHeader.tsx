4468d11a13512c129101f5f5904aa679da8c83a4
Matt Bailey <matt@gpmd.co.uk>
2022-02-24 09:07:35 +0000

    wip

--- components/innerHeader.tsx
+++ components/innerHeader.tsx
@@ -1,7 +1,7 @@
 import { Box, Button, H1, HR, Text } from '@bigcommerce/big-design';
 import { ArrowBackIcon } from '@bigcommerce/big-design-icons';
 import { useRouter } from 'next/router';
-import { useProductList, useWidgetList } from '../lib/hooks';
+import { useProductList } from '../lib/hooks';
 import { TabIds, TabRoutes } from './header';
 
 const InnerHeader = () => {
@@ -9,9 +9,6 @@ const InnerHeader = () => {
     const { pid } = router.query;
     const { list = [] } = useProductList();
     const { name } = list.find(item => item.id === Number(pid)) ?? {};
-    const { uuid } = router.query;
-    const { widgetList = [] } = useWidgetList();
-    const { widgetName } = widgetList.find(item => item.uuid === Number(uuid)) ?? {};
 
     const handleBackClick = () => router.push(TabRoutes[TabIds.PRODUCTS]);
 
@@ -21,7 +18,7 @@ const InnerHeader = () => {
                 <Text bold color="secondary50">Products</Text>
             </Button>
             {name &&
-                <H1>{name}{widgetName}</H1>
+                <H1>{name}</H1>
             }
             <HR color="secondary30" />
         </Box>
